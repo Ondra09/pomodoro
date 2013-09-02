9315c3dfa9b97be96437dff2633f2c57535282b8
Ondrej Pokorny <pokorny.ondrej@gmail.com>
2013-09-02 16:42:01 +0200

    time duration, errors to throw

--- src/pomodoro.d
+++ src/pomodoro.d
@@ -13,6 +13,8 @@ import std.datetime;
 import std.stdio;
 import std.c.process;
 
+import core.time;
+
 import settings;
 
 /**
@@ -23,6 +25,7 @@ import settings;
 int main(string[] args)
 {
 	string gladefile;
+	auto currentTimeT = Clock.currTime();
 
 	Main.init(args);
 
@@ -41,34 +44,49 @@ int main(string[] args)
 
 	if( ! g.addFromFile(gladefile) )
 	{
-		writefln("Oops, could not create Glade object, check your glade file ;)");
+		throw new Exception("Could not create Glade object from file.");
 		exit(1);
 	}
 
 	Window w = cast(Window)g.getObject("Pomodoro");
+	Label timerLabel;
+	Button b;
 
+	bool gladeLoadStatus = true;
 	if (w !is null)
 	{
 		w.setTitle("This is a glade window");
 		w.addOnHide( delegate void(Widget aux){ exit(0); } );
 
-		Button b = cast(Button)g.getObject("button1");
+		b = cast(Button)g.getObject("button1");
 		if (b !is null)
 		{
 			b.addOnClicked( delegate void(Button aux){ exit(0); } );
+		}else
+		{
+			gladeLoadStatus = false;
 		}
 
-		Label timerLabel = cast(Label)g.getObject("label1");
+		timerLabel = cast(Label)g.getObject("label1");
 
 		if (timerLabel !is null)
 		{
 			auto currentTime = Clock.currTime();
-			timerLabel.setLabel(currentTime.toISOExtString());
+			
+			timerLabel.setLabel((currentTime-currentTimeT).toString());
+		}else
+		{
+			gladeLoadStatus = false;
 		}
 	}
 	else
 	{
-		writefln("No window?");
+		gladeLoadStatus = false;
+	}
+
+	if (!gladeLoadStatus)
+	{
+		throw new Exception("Glade file load failure.");
 		exit(1);
 	}
 
